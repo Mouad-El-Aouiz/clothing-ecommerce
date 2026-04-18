0dda45798502d5e5510b41851ff6292184a9ddd3
Mouad-El-Aouiz <mouadelz@icloud.com>
2026-04-18 13:21:54 +0100

    Delete backend/test.py

--- backend/test.py
+++
@@ -1,9 +0,0 @@
-from apps.products.models import ProductImage
-
-for img in ProductImage.objects.all():
-    if 'media/products/' in img.image.name:
-        # Renommer le chemin
-        new_name = img.image.name.replace('media/products/', 'products/')
-        img.image.name = new_name
-        img.save()
-        print(f"✅ Migré: {new_name}")
